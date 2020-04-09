62931b4479fc1d39d3baa61777a85c072d2c2045
hjcasd <2959383735@qq.com>
2020-04-09 16:20:39 +0800

    v1.0-启动页适配全面屏.刘海屏,虚拟导航栏

--- app/src/main/java/com/hjc/base/ui/SplashActivity.java
+++ app/src/main/java/com/hjc/base/ui/SplashActivity.java
@@ -4,14 +4,20 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import com.gyf.immersionbar.ImmersionBar;
 import com.hjc.base.R;
+import com.hjc.base.http.helper.RxHelper;
 import com.hjc.base.http.helper.RxSchedulers;
 import com.hjc.baselib.activity.BaseActivity;
 
 import java.util.concurrent.TimeUnit;
 
+import butterknife.BindView;
 import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 
 /**
  * @Author: HJC
@@ -19,9 +25,22 @@ import io.reactivex.Observable;
  * @Description: 启动页
  */
 public class SplashActivity extends BaseActivity {
+
+    @BindView(R.id.tv_time)
+    TextView tvTime;
+
+    private Disposable disposable1;
+    private Disposable disposable2;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_splash;
+    }
+
+    @Override
+    protected void initImmersionBar() {
+        ImmersionBar.with(this)
+                .init();
     }
 
     @Override
@@ -32,19 +51,42 @@ public class SplashActivity extends BaseActivity {
     @SuppressLint("CheckResult")
     @Override
     public void initData(Bundle savedInstanceState) {
-        Observable.timer(500, TimeUnit.MILLISECONDS)
+        disposable1 = Observable.timer(3, TimeUnit.SECONDS)
                 .compose(RxSchedulers.ioToMain())
-                .subscribe(aLong -> startActivity(new Intent(SplashActivity.this, MainActivity.class)));
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                        finish();
+                    }
+                });
+
+        //倒计时3s
+        disposable2 = Observable.intervalRange(0, 3, 0, 1, TimeUnit.SECONDS)
+                .compose(RxHelper.bind(this))
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        tvTime.setText("倒计时" + (3 - aLong) + "s");
+                    }
+                });
     }
 
     @Override
     public void addListeners() {
-
+        tvTime.setOnClickListener(this);
     }
 
     @Override
     public void onSingleClick(View v) {
-
+        if (v.getId() == R.id.tv_time) {
+            if (disposable1 != null && disposable2 != null) {
+                disposable1.dispose();
+                disposable2.dispose();
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }
     }
 
 }
