0b9c8d012c6415943a3bb1cafd8abe4e2881f010
HJC <2959383735@qq.com>
2021-05-13 23:29:19 +0800

    分支合并测试

--- app/src/main/java/com/hjc/base/ui/MainActivity.java
+++ app/src/main/java/com/hjc/base/ui/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends BaseFragmentActivity<ActivityMainBinding, Comm
         requestPermission();
         registerBroadcastReceiver();
 
-//        anonymousInnerClass();
+        anonymousInnerClass();
     }
 
     /**
