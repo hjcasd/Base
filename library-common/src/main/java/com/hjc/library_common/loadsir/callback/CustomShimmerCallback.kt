a2d0e98c702f9915a983be504832fd633e5d0d51
HUJINCHENG <hujincheng@ceair.com>
2021-09-07 09:51:51 +0800

    骨架屏添加动画

--- library-common/src/main/java/com/hjc/library_common/loadsir/callback/CustomShimmerCallback.kt
+++ library-common/src/main/java/com/hjc/library_common/loadsir/callback/CustomShimmerCallback.kt
@@ -2,7 +2,10 @@ package com.hjc.library_common.loadsir.callback
 
 import android.content.Context
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.hjc.library_common.R
+import com.hjc.library_common.adapter.ShimmerAdapter
 import com.kingja.loadsir.callback.Callback
 
 /**
@@ -18,5 +21,16 @@ class CustomShimmerCallback : Callback() {
 
     override fun onReloadEvent(context: Context, view: View): Boolean {
         return true
+    }
+
+    override fun onViewCreate(context: Context?, view: View?) {
+        super.onViewCreate(context, view)
+        val rvShimmer = view?.findViewById<RecyclerView>(R.id.rv_shimmer)
+        val manager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        rvShimmer?.layoutManager = manager
+
+        val list = mutableListOf("111", "222", "333", "444", "555")
+        val adapter = ShimmerAdapter(list)
+        rvShimmer?.adapter = adapter
     }
 }
